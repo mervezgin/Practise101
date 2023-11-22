03eea8d150002a5af387675976a756e9d76dfa9f
Merve <mervezgin@gmail.com>
2023-11-22 15:44:42 +0300

    Added Data and Connection Strings

--- src/Practise101/Practise101.Api/Models/User.cs
+++ src/Practise101/Practise101.Api/Models/User.cs
@@ -6,5 +6,10 @@
         public string Email { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[32];
         public byte[] PasswordSalt { get; set; } = new byte[32];
+        public string? VerificationToken { get; set; }
+        public DateTime? VerifiedAt { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
     }
 }
+ 
--- src/Practise101/Practise101.Api/Program.cs
+++ src/Practise101/Practise101.Api/Program.cs
@@ -1,4 +1,6 @@
 
+global using Practise101.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
