b991a0c7d490c761710bcfdc4ef72d2c6ac97d4b
Merve <mervezgin@gmail.com>
2023-11-22 15:25:04 +0300

    Some Changes and Added User Model

---
+++ src/Practise101/Practise101.Api/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Practise101.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+    }
+}
