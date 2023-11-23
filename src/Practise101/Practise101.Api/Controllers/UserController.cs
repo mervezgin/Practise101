7986c2b38b1c0ed36beddf174622b0906f78670d
Merve <mervezgin@gmail.com>
2023-11-23 16:05:30 +0300

    Added UserController.cs

---
+++ src/Practise101/Practise101.Api/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practise101.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
