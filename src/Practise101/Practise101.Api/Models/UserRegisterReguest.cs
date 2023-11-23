efea84f6a541e5ad5b606c4d995f13a6c791c4f5
Merve <mervezgin@gmail.com>
2023-11-23 16:01:08 +0300

    Added User Register Request Model

---
+++ src/Practise101/Practise101.Api/Models/UserRegisterReguest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practise101.Api.Models
+{
+    public class UserRegisterReguest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(1)]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = string.Empty;    
+    }
+}
